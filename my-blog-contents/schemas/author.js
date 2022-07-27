7c3678c71b8e263935d0f3387ed159de570bbea1
[SW2/서울/2반]김한얼 <gksdjf80@naver.com>
2022-07-27 22:35:12 +0900

    [create] [author-schemas]

--- my-blog-contents/schemas/author.js
+++ my-blog-contents/schemas/author.js
@@ -7,15 +7,12 @@ export default {
       name: 'name',
       title: 'Name',
       type: 'string',
+      validation: (Rule) => Rule.required(),
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'name',
-        maxLength: 96,
-      },
+      name: 'role',
+      title: 'Role',
+      type: 'string',
     },
     {
       name: 'image',
@@ -24,19 +21,7 @@ export default {
       options: {
         hotspot: true,
       },
-    },
-    {
-      name: 'bio',
-      title: 'Bio',
-      type: 'array',
-      of: [
-        {
-          title: 'Block',
-          type: 'block',
-          styles: [{title: 'Normal', value: 'normal'}],
-          lists: [],
-        },
-      ],
+      validation: (Rule) => Rule.required(),
     },
   ],
   preview: {
@@ -45,4 +30,4 @@ export default {
       media: 'image',
     },
   },
-}
+};
